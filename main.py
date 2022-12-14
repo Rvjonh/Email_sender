1a39dd64aae521add27161f0be4eb040bd055ead
unknown <jonhvelasco3@gmail.com>
2022-12-14 00:23:35 -0400

    allow frontend domain only test1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 FRONTEND_DOMAIN = os.environ.get('FRONTEND_DOMAIN')
 app = Flask(__name__)
-cors = CORS(app, resources={r"/email": {"origins": [FRONTEND_DOMAIN,]}})
+cors = CORS(app, resources={r"/*": {"origins": ["https://rvjonh-portfolio.netlify.app/",]}})
 print("FRONTEND_DOMAIN: ",FRONTEND_DOMAIN)
 
 """ Email configuration """
